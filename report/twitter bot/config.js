855604f8336720964dd57e626b113af32361f35d
karthik-node <karthik.node@gmail.com>
2015-10-06 10:16:06 +0530

    twitter bot example to retweet fav tweets

---
+++ report/twitter bot/config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'xW3Ev6EggSYIrldWgQfqISIJH',
+  consumer_secret:      '	Fv84tnDVopCSCo1G9vtccbw12KnuR4u3eHLDXRcajzmxF3CE6Z',
+  access_token:         '	3791930172-ScemMWcWnPz94qy5GEagPgKzwFOacfs9DV8T4kN',
+  access_token_secret:  '	t9rPfQ3inO6Ty3oEhDoAhP8b9MkmRYebEHNyY1Ry11BlB'
+}
